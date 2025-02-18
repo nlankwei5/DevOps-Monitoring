2807ff28b5514226e92502eec8e6e064c2f2375b
Ebenezer Lamptey <87435572+nlankwei5@users.noreply.github.com>
2025-02-18 10:15:32 +0000

    docker compose created

--- python-api/main.py
+++ python-api/main.py
@@ -1,23 +1,18 @@
 from fastapi import FastAPI, HTTPException
-import os 
-
 
 app = FastAPI()
 
-def read_log_file(file_path):
-    with open(file_path, 'r') as f:
-        return f.readlines()
+LOG_PATH = "/var/log/sysmonitor.log"
 
 @app.get("/logs/")
 async def get_logs():
-    log_path = "/var/log/sysmonitor.log"  # Your specific log file path
     try:
-        logs = read_log_file(log_path)
+        with open(LOG_PATH, "r") as file:
+            logs = file.readlines()
         return {"logs": logs}
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error reading logs: {str(e)}")
 
-    
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
